7b9aa9de4ed48f34604eee3566b28947ce43a2af
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-09-02 22:31:34 +0600

    news headline

--- js/script.js
+++ js/script.js
@@ -1,4 +1,39 @@
-const add=()=>{
-  const r=10+2;
-  console.log(r);
+const loadNewsHead=async()=>{
+const url=`https://openapi.programming-hero.com/api/news/categories`;
+try{
+  const res=await fetch(url);
+  const data=await res.json();
+  displayNewsHead(data.data.news_category);
 }
+catch(error){
+console.log(error);
+}
+
+}
+
+const displayNewsHead=(values)=>{
+console.log(values);
+const newsHead=document.getElementById('news-head');
+values.forEach(value => {
+  console.log(value);
+const div=document.createElement('div');
+div.classList.add('nav');
+  div.innerHTML=`
+
+  <a class="nav-link ms-2" href="#" onclick="loadNews()">${value.category_name}</a>
+  `;
+  newsHead.appendChild(div);
+  
+});
+}
+
+
+const loadNews=async()=>{
+  const url=`https://openapi.programming-hero.com/api/news/category/04`;
+const res=await fetch(url);
+const data=await res.json();
+console.log(data.data);
+
+}
+
+loadNewsHead();
