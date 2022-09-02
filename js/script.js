a7b770161c85aeae85832465085e6fa1d47554c7
Muhibbul Hasan <muhibbulhasan6@gmail.com>
2022-09-03 00:27:30 +0600

    card news add

--- js/script.js
+++ js/script.js
@@ -20,7 +20,8 @@ const div=document.createElement('div');
 div.classList.add('nav');
   div.innerHTML=`
 
-  <a class="nav-link ms-2" href="#" onclick="loadNews()">${value.category_name}</a>
+   <a class="nav-link ms-2" href="#" onclick="loadNews(${value.category_id},'${value.category_name}')">${value.category_name}</a>
+  
   `;
   newsHead.appendChild(div);
   
@@ -28,12 +29,44 @@ div.classList.add('nav');
 }
 
 
-const loadNews=async()=>{
-  const url=`https://openapi.programming-hero.com/api/news/category/04`;
+const loadNews=async(value,n)=>{
+  // console.log(n);
+  const url=`https://openapi.programming-hero.com/api/news/category/0${value}`;
+  // console.log(url);
 const res=await fetch(url);
 const data=await res.json();
-console.log(data.data);
+displayNews(data.data,n);
 
 }
 
+const displayNews=(values,name)=>{
+console.log(values);
+const itemFound=document.getElementById('item-found');
+const catagoryName=document.getElementById('catagory-name');
+itemFound.innerText=values.length;
+catagoryName.innerText=name;
+const newsItems=document.getElementById('news-items');
+newsItems.innerHTML='';
+values.forEach(value => {
+  console.log(value);
+  const div=document.createElement('div');
+  div.classList.add('card','mb-3');
+  div.innerHTML=`
+  <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${value.image_url}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
+          </div>
+        </div>
+  
+  `;
+  newsItems.appendChild(div);
+});
+}
 loadNewsHead();
